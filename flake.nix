12f4745d19b48b7af98bf3e6f4327b2968bceacd
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-10-28 16:41:18 +0700

    path export to make playwright work

--- flake.nix
+++ flake.nix
@@ -66,18 +66,32 @@
           # Frontend shell
           frontend = pkgs.mkShell {
             name = "frontend";
-            buildInputs = [
+            nativeBuildInputs = [
               pkgs.nodejs
               pkgs.eslint
               pkgs.nodePackages.typescript
               pkgs.nodePackages.typescript-language-server
+              pkgs.playwright-driver
             ];
             shellHook = ''
-              if [ ! -d node_modules ]; then
-                echo "Installing npm dependencies..."
-                npm install
-              fi
-              echo "✅ Frontend shell ready (Node)"
+              			  export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+                			  export PLAYWRIGHT_BROWSERS_PATH="$HOME/.cache/playwright-browsers"
+                            export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+                            export QT_QPA_PLATFORM=wayland
+                            export XDG_SESSION_TYPE=wayland
+
+                            if [ ! -d "$PLAYWRIGHT_BROWSERS_PATH" ]; then
+                              echo "Copying Playwright browsers from Nix store..."
+                              mkdir -p "$PLAYWRIGHT_BROWSERS_PATH"
+                              cp -r ${pkgs.playwright-driver.browsers}/* "$PLAYWRIGHT_BROWSERS_PATH" || true
+                            fi
+
+                            if [ ! -d node_modules ]; then
+                              echo "Installing npm dependencies..."
+                              npm install
+                            fi
+
+                            echo "✅ Frontend shell ready (Node + Playwright)"
             '';
           };
 
